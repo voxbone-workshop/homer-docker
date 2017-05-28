3ed1ff453e4609d51e949ef0be5e2aa6e96cf07c
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2017-05-28 22:51:29 +0200

    Update preferences.php

--- everything/data/preferences.php
+++ everything/data/preferences.php
@@ -73,4 +73,7 @@ define('LDAP_USERNAME_ATTRIBUTE_OPEN',"uid=");
 define('LDAP_USERNAME_ATTRIBUTE_CLOSE',"");
 define('LDAP_USERLEVEL',3); 
 */
+define('RTCP_TABLE_PARTITION', 0);
+define('SYSLOG_ENABLE',0);
+
 ?>
